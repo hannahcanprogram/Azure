839c673fe672e9b4e46fe98b9c4580f0bdda7f4f
Wu Han <wuhan@WudeMacBook-Pro.local>
2021-07-16 10:03:42 -0400

    update 2rd lad and assignment

--- lab1_Han_Wu_0714.sql
+++ lab1_Han_Wu_0714.sql
@@ -1,40 +1,44 @@
 
 --1.All customers(names), and their postal cities, which brought more than 2000 "toys" (as stock group) in 2016
+
+/*
 select * from Sales.Customers
 select * from Sales.OrderLines
 select * from Sales.Orders
 select * from Application.Cities
 select * from Warehouse.StockGroups
 select * from Warehouse.StockItemStockGroups
+*/
 
 with cte1 as
 (
 select si.StockItemID
 from Warehouse.StockItems si left join Warehouse.StockItemStockGroups sig on si.StockItemID = sig.StockItemID
                             left join Warehouse.StockGroups sg on si.StockItemID = sg.StockGroupID
-                            and sg.StockGroupName = 'Toys'
+where sg.StockGroupName = 'Toys'
 )
  
-select temp.CustomerName, ct.CityName
-from 
+select temp.CustomerName, temp.CityName
+from
 (
-select c.CustomerID, c.CustomerName, c.DeliveryCityID, sum(ol.Quantity) as quantity
+select c.CustomerID, c.CustomerName, ct.CityName, sum(ol.Quantity) as quantity
 from Sales.OrderLines ol left join Sales.Orders o on ol.OrderID = o.OrderID
                          left join cte1 on ol.StockItemID = cte1.StockItemID
 						 right join Sales.Customers c on c.CustomerID = o.CustomerID
-						 and datepart(year,o.OrderDate)= 2016
-group by c.CustomerID, c.CustomerName , c.DeliveryCityID
+						 right join Application.Cities ct on ct.CityID = c.PostalCityID
+where datepart(year,o.OrderDate)= 2016
+group by c.CustomerID, c.CustomerName , ct.CityName
 having sum(ol.Quantity) > 2000
-) temp 
-       left join Application.Cities ct 
-	   on temp.DeliveryCityID = ct.CityID
+) temp
 
 --2.
+/*
 select * from Sales.OrderLines
 select * from Sales.Orders
 select * from Purchasing.PurchaseOrderLines
 select * from Purchasing.PurchaseOrders
 select * from Warehouse.StockItems
+*/
 
 select si.StockItemName
 from
@@ -62,11 +66,11 @@ Warehouse.StockItems si left join
                                    group by ol.StockItemID
                                   ) sal_temp 
                             on si.StockItemID = sal_temp.StockItemID 
-		                and pur_temp.pur_quantity > sal_temp.sal_quantity
+where pur_temp.pur_quantity > sal_temp.sal_quantity
 
 
 --3.
-
+/*
 select * from Sales.SpecialDeals
 select * from Warehouse.StockItemStockGroups
 select * from Sales.Orders
@@ -75,23 +79,34 @@ Select * from Warehouse.StockItems
 Select * from Warehouse.StockGroups
 select * from Sales.Customers
 select * from Sales.CustomerCategories
-
-
-
+select * from Sales.BuyingGroups
+*/
 
 with cte1 as 
 (
-select sig.StockItemID as disc_stockItemID
+select sig.StockItemID
 from Warehouse.StockItemStockGroups sig 
 where sig.StockGroupID in (select distinct sd.StockGroupID from Sales.SpecialDeals sd)
 )
 
-select ol.OrderLineID, ol.OrderID, ol.StockItemID, ol.Quantity, ol.UnitPrice, o.OrderDate, c.BuyingGroupID
-       case when ol.StockItemID in (select disc_stockItemID from cte1)
-	           and c.BuyingGroupID in (select sd.BuyingGroupID from Sales.SpecialDeals sd)
-			then 
+select sum( ol.Quantity*ol.UnitPrice*sd.DiscountPercentage)  as loss_profit
+
 from Sales.OrderLines ol left join Sales.Orders o on ol.OrderID = o.OrderID
                          left join Sales.Customers c on o.CustomerID = c.CustomerID 
-						 left join Sales.SpecialDeals sd on c.BuyingGroupID = sd.BuyingGroupID
+						 join warehouse.StockItemStockGroups ssg on ssg.StockItemID = ol.StockItemID
+						 join Sales.SpecialDeals sd on sd.BuyingGroupID = c.BuyingGroupID and sd.StockGroupID = ssg.StockGroupID
+where c.CustomerID in (
+          select c.CustomerID
+		  from Sales.Customers c 
+		  where c.BuyingGroupID in (select * from (select sd.BuyingGroupID as disc_buyinggoupID
+                                    from  Sales.SpecialDeals sd  ) temp)  )
+	  and 
+	  ol.StockItemID in ( select * from cte1 ) 
+	  and
+	  o.OrderDate between sd.StartDate and sd.EndDate
+	      
+
+
+
 
 
